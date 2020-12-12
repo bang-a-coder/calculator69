571f75d0643acf6fc66564a80e6da704ac197ad0
Nikitas Filosofof <nikitas4717@gmail.com>
2020-12-12 01:19:36 +0100

    barely any concept

--- script.js
+++ script.js
@@ -8,15 +8,30 @@ let clearButton = document.getElementById(`clear`)
 let backspaceButton = document.getElementById(`backspace`)
 let equalButton = document.getElementById(`equal`)
 
+let toDo = [3,5*3,-10/2]   //3+5*3-10/2
+let input = ``// temporary thingy to keep inputs between operators
+
 for (i = 0; i < numButtons.length; i++) {                       //Number eventListerner set
     numButtons[i].addEventListener(`click`, function() {
         inputScreen.innerHTML += `${this.id.toString()}`
+        input += this.id
+        console.log(input)
     })
 }
 
 for (i = 0; i < operButttons.length; i++){                      //Operator eventListenser set
     operButttons[i].addEventListener(`click`, function() {
         inputScreen.innerHTML += `${this.id.toString()}`
+
+        if (this.id === `+`) {
+            toDo.push(input.slice(0,-1))
+            input = ``
+        }
+
+
+
+
+
     })
 }
 
@@ -24,8 +39,12 @@ clearButton.addEventListener(`click`, function(){               //Clean screen e
     inputScreen.innerHTML = ''
 })
 
+equalButton.addEventListener(`click`, function() {
+    operate()
+})
 
 
-function operate(calculation){
-    
+
+function operate(calculation){  
+    inputScreen.innerHTML += null
 }
